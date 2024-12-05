031b1f0b785ed9506d4ff515d258a1d031443bc9
AnimCooch <griffinvand@gmail.com>
2024-12-05 13:10:56 -0500

    More stuff

--- SentimentAnalysis/main.py
+++ SentimentAnalysis/main.py
@@ -1,11 +1,8 @@
 import pandas as pd
 import tkinter as tk
 
-def AcceptInput():
-    user_input = entry.get()
-    output_label = (AnalyzeSentiment(user_input))
-    entry.delete(0, tk.END)
-
+"""Compares given input against two lists of words(positive and negative) and determines if
+the input is positive or negative"""
 def AnalyzeSentiment(userInput):
     pw = pd.read_csv("SentimentAnalysis/POSITIVE_WORDS.csv", header=None)
     nw = pd.read_csv("SentimentAnalysis/NEGATIVE_WORDS.csv", header=None)
@@ -22,26 +19,65 @@ def AnalyzeSentiment(userInput):
             if word.lower() == nWord:
                 nScore += 1
     
-    return (f'Positive: {pScore}, Negative: {nScore}')
+    """Stores the ratio of positive to negative words"""
+    pnRatio = 0
+    if(pScore != 0 and nScore != 0):
+        pnRatio = pScore / nScore
+
+    """If the ratio of positive to negative words is within a given range
+    it's safer to assume the statement is not specifically negative or positive"""
+    if pScore == nScore or (pnRatio > 0.8 and pnRatio < 1.2):
+        return ('neutral')
+    elif pScore > nScore:
+        return ('positive')
+    else:
+        return ('negative')
+    
+
+"""Updates the display to show the last statement typed by the user"""
+def UpdateLastUserInput(userInput):
+    youtyped_label.config(text='You typed:')
+    lastinput_label.config(text=f'{userInput}')
+    thisstatement_label.config(text='This statement might be:')
+    output_label.config(text=(AnalyzeSentiment(userInput)))
 
 
+"""Reads the user entry and sends it to the analyze sentiment function"""
+def AcceptInput():
+    user_input = entry.get()
+    UpdateLastUserInput(user_input)
+    entry.delete(0, tk.END)
+
+"""Creates new tk instance"""
 root = tk.Tk()
-root.title("Enter Here")
-root.geometry("400x200")
+root.title("Sentiment Analysis Chamber")
+root.geometry("600x400")
 
-
+"""Instructions label"""
 label = tk.Label(root, text="Type anything and I will analyze it")
 label.pack(pady=10)
 
+"""Area for user to enter their statement"""
 entry = tk.Entry(root, width=30)
 entry.pack(pady=5)
 
+"""Button to start analysis"""
 button = tk.Button(root, text="Analyze Sentiment", command=AcceptInput)
 button.pack(pady=10)
 
-output_label = tk.Label(root, text="", fg="blue")
-output_label.pack(pady=10)
+"""Displays the last typed statment"""
+youtyped_label = tk.Label(root, text="")
+youtyped_label.pack(pady=10)
+lastinput_label = tk.Label(root, text = "", fg="blue")
+lastinput_label.pack(pady=5)
 
+"""Where the analysis results are displayed"""
+thisstatement_label = tk.Label(root, text="")
+thisstatement_label.pack(pady=10)
+output_label = tk.Label(root, text="", fg="orange")
+output_label.pack(pady=5)
+
+"""Continues running until the user exits"""
 root.mainloop()
 
 
