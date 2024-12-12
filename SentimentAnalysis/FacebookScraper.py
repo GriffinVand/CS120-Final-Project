5726f8ea1ad667c69d21bfc1a70799a874286844
gforbes9909 <gforbes@clarku.edu>
2024-12-12 13:25:53 -0500

    Create FacebookScraper.py

---
+++ SentimentAnalysis/FacebookScraper.py
@@ -0,0 +1,29 @@
+from playwright.sync_api import sync_playwright
+
+def scrape_facebook(username, password, target_url):
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=False, slow_mo=500)
+        context = browser.new_context()
+        page = context.new_page()
+
+        page.goto("https://www.facebook.com/login")
+
+        page.fill("#email", username)
+        page.fill("#pass", password)
+        page.click("button[name='login']")
+
+        page.wait_for_selector("[aria-label='Search Facebook']")
+
+        page.goto(target_url)
+
+        page.wait_for_selector("[data-pagelet^='FeedUnit']")
+
+        posts = page.locator("[data-pagelet^='FeedUnit']")
+        posts_text = []
+        for i in range(posts.count()):
+            post = posts.nth(i).text_content()
+            posts_text.append(post)
+
+        browser.close()
+
+        return posts_text
